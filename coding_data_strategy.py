26f31e089e55e8c196ce4d3c676d0d1f8c5e4bca
Zhen Li <zl694@cornell.edu>
2018-09-28 17:45:41 -0400

    ### [0928 17:40] Zhen Li msg categorized by strategy structure: ```json {     "strategy_1":[         {"Num": 1202,         "Request": "...",         "ch_mean": 6.44444465637207,         "en_mean": 4.857142925262451},         {"Num": 1258,         "Request": "...",         "ch_mean": 5.25,         "en_mean": 3.0}     ],     "strategy_2":[         {           //...         },         {           //...         }     ]     //... } ```

--- coding_data_strategy.py
+++ coding_data_strategy.py
@@ -4,10 +4,37 @@ from politeness.api_util import get_scores_and_strategies
 #for text in TEST_TEXTS:
 #  print(get_scores_and_strategies(text))
 
+import numpy as np
 import pandas as pd
 import json
 
-df = pd.read_csv("politeness_coding_data.csv", encoding="utf-8", index_col=[0])
+df = pd.read_csv("PolCodings600.csv", encoding="utf-8")
 
-for row in df.iterrows():
-  
+result = {}
+
+for i, row in df.iterrows():
+  Num = row["Num"]
+  Request = row["Request"]
+  ret = get_scores_and_strategies(Request)
+  en_mean = np.asscalar(np.nanmean(np.array((row["EN1":"EN31"]).values, dtype=np.float32)))
+  ch_mean = np.asscalar(np.nanmean(np.array((row["CH1":"CH21"]).values, dtype=np.float32)))
+  for s in ret["strategies"]:
+    if s not in result:
+      result[s] = []
+    result[s].append(
+      {
+        "Num": Num,
+        "Request": Request,
+        "en_mean": en_mean,
+        "ch_mean": ch_mean,
+      }
+    )
+  if i % 20 == 0:
+    print(i)
+
+with open("sort_in_strategies.json", "w", encoding="utf-8") as fout:
+  json.dump(result, fout)
+
+import pprint
+
+pprint.pprint(result)
