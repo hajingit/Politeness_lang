2f30c2340b3ceec8a2b0c46beb6309f36d233e73
Zhen Li <zl694@cornell.edu>
2018-11-05 17:18:17 -0500

    data reader

--- ten_models/data_reader.py
+++ ten_models/data_reader.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import pickle
+import random
 
-def read(feature_path="train_features.pkl", label_path="train.csv"):
+def _read(feature_path="train_features.pkl", label_path="train.csv"):
   with open(feature_path, "rb") as fin:
     pkl = pickle.load(fin)
   df = pd.read_csv(label_path, index_col=0)
+  X = pkl.toarray()
+  Y = df.loc[:, "ch_mean":"en_mean"].values
+  return np.concatenate((X, Y), axis=1)
 
-  return {
-    "X": pkl,
-    "Y": df.loc[:, "ch_mean":"en_mean"].values
-  }
+def read(feature_path="train_features.pkl", label_path="train.csv"):
+  # arr = _read(feature_path, label_path)
+  # return {
+  #   "X": arr[:, :-2]
+  #   "Y": arr[:, -2:]
+  # }
+  return _read(feature_path, label_path)
