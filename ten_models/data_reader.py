14ad81f4001edc156986fee44c1bdf2e24556d16
Zhen Li <zl694@cornell.edu>
2018-11-05 17:06:57 -0500

    data reader with full features

--- ten_models/data_reader.py
+++ ten_models/data_reader.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import pickle
 
-def read(path="train_strategy.csv"):
-  df = pd.read_csv(path, index_col=0)
-  X = df.loc[:, "2nd person":].values
-  Y = df.loc[:, "ch_mean":"en_mean"].values
+def read(feature_path="train_features.pkl", label_path="train.csv"):
+  with open(feature_path, "rb") as fin:
+    pkl = pickle.load(fin)
+  df = pd.read_csv(label_path, index_col=0)
+
   return {
-    "X": X,
-    "Y": Y,
+    "X": pkl,
+    "Y": df.loc[:, "ch_mean":"en_mean"].values
   }
