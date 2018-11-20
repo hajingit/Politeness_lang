a3837fcbd2f13a5635d46e14c3516bb5f1c24776
Zhen Li <zl694@cornell.edu>
2018-11-19 22:26:14 -0500

    k_fold cross validation, threshold

--- ten_models/data_reader.py
+++ ten_models/data_reader.py
@@ -3,13 +3,30 @@ import pickle
 import random
 import numpy as np
 
-def _read(feature_path="train_features.pkl", label_path="train.csv"):
-  with open(feature_path, "rb") as fin:
-    pkl = pickle.load(fin)
-  df = pd.read_csv(label_path, index_col=0)
-  X = pkl.toarray()
-  Y = df.loc[:, "ch_mean":"en_mean"].values
-  return np.concatenate((X, Y), axis=1)
+def _read(feature_path="train_features.pkl", label_path="train.csv", filter_mode="", filter_val=0):
+    with open(feature_path, "rb") as fin:
+        pkl = pickle.load(fin)
+    df = pd.read_csv(label_path, index_col=0)
+    X = pkl.toarray()
+    Y = df.loc[:, "ch_mean":"en_mean"].values
+    arr = np.concatenate((X, Y), axis=1)
+    
+    if filter_mode == "value":
+        print("filtering data by absolute value")
+        arr = arr[(np.abs(arr[:,-2])>=filter_val) & (np.abs(arr[:,-1])>=filter_val)]
+    elif filter_mode == "percentile":
+        print("filtering data by percentile")
+        assert filter_val % 1 == 0 and 0 < filter_val < 100, "invalid percentile value"
+        if filter_val > 50:
+            filter_val = 100 - filter_val
+        lower1 = np.percentile(arr[:, -1], filter_val)
+        upper1 = np.percentile(arr[:, -1], 100-filter_val)
+        lower2 = np.percentile(arr[:, -2], filter_val)
+        upper2 = np.percentile(arr[:, -2], 100-filter_val)
+        
+        arr = arr[np.logical_and(np.logical_or(arr[:,-1]>=upper1, arr[:,-1]<=lower1), np.logical_or(arr[:,-2]>=upper2, arr[:,-2]<=lower2))]
+    
+    return arr
 
 def read(feature_path="train_features.pkl", label_path="train.csv"):
   # arr = _read(feature_path, label_path)
@@ -18,3 +35,14 @@ def read(feature_path="train_features.pkl", label_path="train.csv"):
   #   "Y": arr[:, -2:]
   # }
   return _read(feature_path, label_path)
+
+def k_fold_cross_val(feature_path="train_features.pkl", label_path="train.csv", k=10, seed=777, filter_mode="", filter_val=0):
+    arr = _read(feature_path, label_path, filter_mode, filter_val)
+    random.seed(seed)
+    
+    all_ind = list(range(len(arr)))
+    random.shuffle(all_ind)
+    for i in range(k):
+        test_ind = set(all_ind[i::k])
+        train_ind = set(all_ind) - test_ind
+        yield arr[list(train_ind)], arr[list(test_ind)]
